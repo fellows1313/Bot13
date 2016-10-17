09de6c8459d8217af92d7978be7d54523b964408
1313 Innovation <fellows@1313innovation.com>
2016-10-17 16:10:38 -0400

    Revised how rent options are read in from API.ai

--- Bot13.py
+++ Bot13.py
@@ -19,9 +19,9 @@ class RentOptions:
         self.rentType = rentType
         self.rentDescription = rentDescription
 
-rentOptions.append(RentOptions("DayPass", "Join us for a day for only $10.00!"))
-rentOptions.append(RentOptions("HotDesk", "Come and go as you please for $75.00 a month"))
-rentOptions.append(RentOptions("DedicatedDesk", "Your own personal desk available anytime for $300.00 a month"))
+rentOptions.append(RentOptions("DayPass", "Day passes are available for only $10.00!"))
+rentOptions.append(RentOptions("HotDesk", "A Hot Desk lets you come and go as you please for $75.00 a month"))
+rentOptions.append(RentOptions("DedicatedDesk", "A Dedicated Desk is your own personal desk available anytime for $300.00 a month"))
 rentOptions.append(RentOptions("OfficeSpace", "Need a home for your startup? Office space starting as low as $500.00 a month"))
 
 
@@ -49,15 +49,17 @@ def processRequest(req):
 def generateResult(req):
     result = req.get("result")
     parameters = result.get("parameters")
-    rentType = parameters.get("DayPass") # This would be your value DayPass, HotDesk, etc.
 
     # Find the matching type in our array of rentOptions
-    speechResponse = "Issue finding info about " + rentType
-    textResponse = "Issue finding info about " + rentType
+    speechResponse = "Sorry something went wrong! Try again."
+    textResponse = "Sorry something went wrong! Try again."
+
     for rentOption in rentOptions:
-        if (rentOption.rentType == rentType):
-            speechResponse = rentOption.rentType + " " + rentOption.rentDescription
-            textResponse = rentOption.rentType + " - " + rentOption.rentDescription
+        rentType = rentOption.rentType
+        # Match the corresponding rentType with the corresponding parameter value
+        if (rentType == parameters.get(rentType)):
+            speechResponse = rentOption.rentDescription
+            textResponse = rentOption.rentDescription
             break
 
 
